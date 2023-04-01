2c1b20fb058f358a51e6f1514b88eed616817984
teodormititelu <72453179+teodormititelu@users.noreply.github.com>
2023-04-01 16:52:46 +0300

    Added DTOs

---
+++ DTOs/UserDto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanetPals___backend.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CompanyId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Score { get; set; }
+        public List <bool> Achievements { get; set; }
+    }
